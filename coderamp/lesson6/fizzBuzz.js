1ab0ba9d7db0fa4870a7593a9f16cf34b31a91ef
Taylor Hogan <taylorb.hogan@gmail.com>
2022-05-18 14:15:00 -0700

    Init

---
+++ coderamp/lesson6/fizzBuzz.js
@@ -0,0 +1,61 @@
+// Fizz
+// Take in a number x
+// For all numbers between 1 and x inclusive: log the number and "Fizz" if the number is divisible by 3.
+function Fizz(x) {
+
+  for (let y=1; y<=x; y++) {
+   if (y % 3 === 0) {
+      console.log(y, "Fizz");
+    }
+  }
+}
+console.log("Fizz:")
+Fizz(15);
+/*
+  3 Fizz
+  6 Fizz
+  9 Fizz
+  12 Fizz
+  15 Fizz
+*/
+
+
+// Buzz
+// Take in a number x
+// For all numbers between 1 and x inclusive: log the number and "Buzz" if the number is divisible by 5.
+function Buzz(x) {
+
+  for (let y=1; y<=x; y++) {
+   if (y % 5 === 0) {
+      console.log(y, "Buzz");
+    }
+  }
+}
+
+console.log("Buzz:")
+Buzz(15);
+/*
+  5 Buzz
+  10 Buzz
+  15 Buzz
+*/
+
+
+// FizzBuzz
+// Take in a number x
+// For all numbers between 1 and x inclusive: log the number and "Fizz" only if the number is divisible by 3, the number and "Buzz" only if the number is divisible by 5, and log the number and "FizzBuzz" only if the number is divisible by both 5 and 3.
+function fizzBuzz(x) {
+
+  for (let y=1; y<=x; y++) {
+    if (y % 3 === 0 && y % 5 === 0) {
+      console.log(y, "FizzBuzz");
+    } else if (y % 3 === 0) {
+      console.log(y, "Fizz");
+    } else if (y % 5 === 0) {
+      console.log(y, "Buzz");
+    }
+  }
+}
+
+console.log("FizzBuzz:")
+fizzBuzz(15);
