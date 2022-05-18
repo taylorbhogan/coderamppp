06efef200dce2848ac0f55f8b42dfa2b1b324bc1
Taylor Hogan <taylorb.hogan@gmail.com>
2022-05-18 14:41:39 -0700

    FizzBuzz solutions

--- coderamp/lesson6/fizzBuzz.js
+++ coderamp/lesson6/fizzBuzz.js
@@ -3,13 +3,8 @@
 // For all numbers between 1 and x inclusive: log the number and "Fizz" if the number is divisible by 3.
 function Fizz(x) {
 
-  for (let y=1; y<=x; y++) {
-   if (y % 3 === 0) {
-      console.log(y, "Fizz");
-    }
-  }
 }
-console.log("Fizz:")
+console.log("Executing Fizz:")
 Fizz(15);
 /*
   3 Fizz
@@ -19,20 +14,29 @@ Fizz(15);
   15 Fizz
 */
 
+///////////////////////////////// SOLUTION /////////////////////////////////
+// function Fizz(x) {
+
+//   for (let y=1; y<=x; y++) {
+//    if (y % 3 === 0) {
+//       console.log(y, "Fizz");
+//     }
+//   }
+// }
+///////////////////////////////// SOLUTION /////////////////////////////////
+
+
+
+
 
 // Buzz
 // Take in a number x
 // For all numbers between 1 and x inclusive: log the number and "Buzz" if the number is divisible by 5.
 function Buzz(x) {
 
-  for (let y=1; y<=x; y++) {
-   if (y % 5 === 0) {
-      console.log(y, "Buzz");
-    }
-  }
 }
 
-console.log("Buzz:")
+console.log("Executing Buzz:")
 Buzz(15);
 /*
   5 Buzz
@@ -40,22 +44,43 @@ Buzz(15);
   15 Buzz
 */
 
+///////////////////////////////// SOLUTION /////////////////////////////////
+// function Buzz(x) {
+
+//   for (let y=1; y<=x; y++) {
+//    if (y % 5 === 0) {
+//       console.log(y, "Buzz");
+//     }
+//   }
+// }
+///////////////////////////////// SOLUTION /////////////////////////////////
+
+
+
+
 
 // FizzBuzz
 // Take in a number x
 // For all numbers between 1 and x inclusive: log the number and "Fizz" only if the number is divisible by 3, the number and "Buzz" only if the number is divisible by 5, and log the number and "FizzBuzz" only if the number is divisible by both 5 and 3.
 function fizzBuzz(x) {
 
-  for (let y=1; y<=x; y++) {
-    if (y % 3 === 0 && y % 5 === 0) {
-      console.log(y, "FizzBuzz");
-    } else if (y % 3 === 0) {
-      console.log(y, "Fizz");
-    } else if (y % 5 === 0) {
-      console.log(y, "Buzz");
-    }
-  }
 }
 
-console.log("FizzBuzz:")
+console.log("Executing FizzBuzz:")
 fizzBuzz(15);
+
+
+///////////////////////////////// SOLUTION /////////////////////////////////
+// function fizzBuzz(x) {
+
+//   for (let y=1; y<=x; y++) {
+//     if (y % 3 === 0 && y % 5 === 0) {
+//       console.log(y, "FizzBuzz");
+//     } else if (y % 3 === 0) {
+//       console.log(y, "Fizz");
+//     } else if (y % 5 === 0) {
+//       console.log(y, "Buzz");
+//     }
+//   }
+// }
+///////////////////////////////// SOLUTION /////////////////////////////////
