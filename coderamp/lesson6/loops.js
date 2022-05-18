668a5e56422f89d3d356c923049a28f3bceea946
Taylor Hogan <taylorb.hogan@gmail.com>
2022-05-18 14:18:07 -0700

    Setup

---
+++ coderamp/lesson6/loops.js
@@ -0,0 +1,13 @@
+// Print number 0 - 10
+// We can do everything by hand:
+// console.log(0)
+// console.log(1)
+
+const arr = [1, 2, 3]
+
+// Or we can write a loop to handle that repeatable work for us:
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(i)
+// }
+
+
