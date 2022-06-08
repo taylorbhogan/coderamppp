6fd2c689d883808d8234af38a9565d5c4ce71831
Taylor Hogan <taylorb.hogan@gmail.com>
2022-06-08 12:03:43 -0700

    LastWordLength

--- coderamp/lesson12/additional.js
+++ coderamp/lesson12/additional.js
@@ -10,17 +10,29 @@ e.g.
 
 */
 
-function vowelRemover (str) {
-  const dict = {a: 1, e: 1, i: 1, o: 1, u: 1, y: 1, A: 1, E: 1, I: 1, O: 1, U: 1, Y: 1}
-  const res = []
-  for (let i = 0; i < str.length; i++){
-    if (str[i] in dict === false){
-      res.push(str[i])
+function vowelRemover(str) {
+  const dict = {
+    a: 1,
+    e: 1,
+    i: 1,
+    o: 1,
+    u: 1,
+    y: 1,
+    A: 1,
+    E: 1,
+    I: 1,
+    O: 1,
+    U: 1,
+    Y: 1,
+  };
+  const res = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in dict === false) {
+      res.push(str[i]);
     }
   }
-  return res.join("")
+  return res.join("");
 }
-
 
 /* How long is the longest sentence?
 
@@ -35,17 +47,56 @@ const sentences = [
 
 */
 
-function longestSentenceFinder (arr) {
+function longestSentenceFinder(arr) {
   let max = -Infinity;
 
-  for (let i = 0; i < arr.length; i++){
-    const sentence = arr[i]
+  for (let i = 0; i < arr.length; i++) {
+    const sentence = arr[i];
 
-    if (sentence.split(" ").length > max){
-      max = sentence.split(" ").length
+    if (sentence.split(" ").length > max) {
+      max = sentence.split(" ").length;
     }
   }
   return max;
 }
 
-console.log(longestSentenceFinder(sentences))
+/* How long is the last word in a sentence?
+
+* Write a function that when given a sentence, returns the number of characters found in its last word.
+* Bonus: exclude the punctuation marks ".", "!", and "?" from your character count, when applicable.
+
+e.g.
+"Hello, world" => 5
+"This Function is Powered by A Great Language Called JavaScript" => 10
+Acceptable: "BANANAS OF THE WORLD, UNITE!" => 6
+Bonus: "BANANAS OF THE WORLD, UNITE!" => 5
+*/
+
+// const lastWordLength = str => {
+//   const sentence = str.trim()
+//   let count = 0;
+//   for (let i = sentence.length - 1; i >= 0; i--){
+//     const char = sentence[i];
+//     if (char === " "){
+//       return count;
+//     } else {
+//       count++
+//     }
+//   }
+// }
+const lastWordLength = (str) => {
+  const sentence = str.trim();
+  const symbols = { "!": true, ".": true, "?": true };
+  let count = 0;
+  for (let i = sentence.length - 1; i >= 0; i--) {
+    const char = sentence[i];
+    if (char === " ") {
+      return count;
+    } else if (char in symbols) {
+      continue;
+    } else {
+      count++;
+    }
+  }
+};
+
